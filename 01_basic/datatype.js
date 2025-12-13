17ac5bbc0fe8d05a3051a949b3c26edd0f5ac63b
parul62b <parulbuwade2603@gmail.com>
2025-12-13 06:43:16 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version through acient version mean ye ancient version to suppoert krega but use only newer version for syntax
+
+// alert(3+3)   we are using node.js ,not browser
+// ecma and mdn
+
+let name = "abc"
+let age  =16
+let isLogin = true
+
+// datatypes :-> number => 2 power 53
+// bigint => large value
+//  string => " "(preferred) or ''
+// boolean => true/false
+//  null => standalone value (empty)
+//  undefined => value defined nhi h or value assign nh hui
+// symbol => for unique values
+
+// object 
+
+console.log(typeof age );
+console.log(typeof null);       // object 
+console.log(typeof undefined);  // undefined
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,28 @@
 console.log("fpp");
+const accountId = 123;
+let accountEmail ="wse123@gmail.com";
+var accountName = "jaipur";
+accountCity = "abc"
+
+// accountId = 234; not allowed for constant
+
+
+console.log(accountId);
+console.log(accountName);
+
+let accountState;//undefined
+
+
+accountEmail ="abs234@gmail.com";
+accountName = "indore";
+
+// for printing all thing at once 
+console.table([accountName,accountId,accountEmail,accountCity,accountState]);
+
+
+/* conclusion: # let,const ka use kre ;
+# var ka use nhi bcoz easy to change h ;
+# some variables w/o assign bhi use hota jase accountCity pr hme nhi krna ;
+# use table if want to print too many var's
+# const change nhi hota but baki lastest assign pr kaam krte h 
+*/
