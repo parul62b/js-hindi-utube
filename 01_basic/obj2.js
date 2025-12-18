0d7f0eb82e96f18093b5b5ddd1d81f9fe1b6482b
parul62b <parulbuwade2603@gmail.com>
2025-12-18 03:45:49 +0000

    obj2

---
+++ 01_basic/obj2.js
@@ -0,0 +1,39 @@
+// object declare by singleton
+const tinderUser = new Object () //declare as singleton {}
+// const tinderUser = {}        //{}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);    //{ id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+const regularUser = {
+email: "some@gmail.com",
+fullname: {
+userfullname: {
+firstname: "hitesh",
+lastname: "choudhary"
+}
+}
+}
+console.log(regularUser.fullname);   //{ userfullname: { firstname: 'hitesh', lastname: 'choudhary' } }
+console.log(regularUser.fullname.userfullname); // { firstname: 'hitesh', lastname: 'choudhary' } 
+// ? is val not exist 
+
+const obj1 = {1: "a" , 2: "b"}
+const obj2 = {3: "a" , 4: "b"}
+const obj3 = {5: "a" , 6: "b"}
+
+// const obj4 ={obj1,obj2}                           //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+ const obj4 = Object.assign({} ,obj1 ,obj2 ,obj3)     //{  '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+//const obj4 = {...obj1,...obj2}            //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+console.log(obj4);
+
+
+console.log(tinderUser);                                        //{ id: '123abc', name: 'Sammy', isLoggedIn: false }
+console.log(Object.keys (tinderUser));                          //[ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values (tinderUser));                   //[ '123abc', 'Sammy', false ]
+console.log(Object.entries (tinderUser));                    //[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));      //true
