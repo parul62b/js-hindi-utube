42686007f61e00c6d001333a6e8f7abe90a6579b
parul62b <parulbuwade2603@gmail.com>
2025-12-18 04:11:14 +0000

    obj3

--- 01_basic/obj3.js
+++ 01_basic/obj3.js
@@ -1,1 +1,28 @@
- 
+
+const  course = {
+    coursename :"js in hindi",
+    price : "999",
+    courseInstructor:"hitesh"
+}
+
+//course.courseInstructor
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);          //hitesh
+console.log(instructor);            //hitesh
+
+
+// destructuring with nested objects
+//{
+//"name": "hitesh",
+//"coursename": "js in hindi",
+//"price": "free"
+// }
+
+/*
+//arrays destructuring
+[
+{},
+{},
+{}
+] 
+*/
